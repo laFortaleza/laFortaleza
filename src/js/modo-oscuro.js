0f3e9fa434700b9300a2031c194cdec55ca2af7a
yarygarcia <yarygarciaq@hotmail.com>
2022-08-12 17:25:30 -0500

    Boton dark en todas las paginas

--- src/js/modo-oscuro.js
+++ src/js/modo-oscuro.js
@@ -1,33 +1,11 @@
-// const head = document.querySelector(".carrusel");
-// let grandeC = document.querySelector(".grande");
-// const puntos = document.querySelector(".puntos")
-// const frutas = document.querySelector(".frutas")
-// const footer = document.querySelector("footer");
-// const navMenu = document.querySelector(".menu-nav")
-// const videoC = document.querySelector(".video")
-// const reproduccion = document.querySelector(".reproduccionVideo")
-// const verduras = document.querySelector(".verduras")
-// const btnServicios = document.querySelector(".btnServicios")
-// const titleReservas = document.querySelector(".titleR")
-
 
 const boton = document.querySelector(".clarOscuro");
 
 accion =(e) =>{
     e.preventListener;
 
-        // head.classList.toggle("verde-dark");
-        // grandeC.classList.toggle("verde-dark");
-        // puntos.classList.toggle("verde-dark");
-        // frutas.classList.toggle("verde-dark")
-        // footer.classList.toggle("verde-dark");
         document.body.classList.toggle("verde-dark")
         boton.classList.toggle("active")
-        // navMenu.classList.toggle("blanco")
-        // videoC.classList.toggle("verde-pino");
-        // reproduccion.classList.toggle("verde-pino")
-        // verduras.classList.toggle("verde-pino")
-        // btnServicios.classList.toggle("btnServices")
 
         if(document.body.classList.contains('verde-dark')){
             localStorage.setItem('dark-mode', 'true')
@@ -38,9 +16,6 @@ accion =(e) =>{
             console.log('falsee')
         }
     
-        // titleReservas.classList.toggle("verde-pino")
-    
-
 }
 if(localStorage.getItem('dark-mode') ==='true'){
     document.body.classList.add("verde-dark")
